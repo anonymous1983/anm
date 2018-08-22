d6785b3f3e50844238df7c0a493126148cc73913
ANIS ABID <anis.abid@atexo.com>
2018-08-22 16:58:33 +0200

    New request design

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,44 @@
 (function ($) {
 
+    $(document).ready(function () {
 
-    $('[data-toggle="tooltip"]').tooltip();
+        $('[data-toggle="tooltip"]').tooltip();
 
-    $('.circle').each(function () {
-        var $circle = $(this);
-        $circle.circleProgress().on('circle-animation-progress', function (event, progress) {
-            $(this).find('strong').html($.number( Math.round($circle.data('number') * progress), 0, ',', ' ' ));
-        }).on('circle-animation-end', function () {
-            $(this).find('strong').html($.number( $circle.data('number'), 0, ',', ' ' ));
-        })
-    });
+        $('.carousel-logos').carousel('cycle');
 
-
-    $('#numberDematSimple').animateNumber(
-        {number: 100},
-        1000
-    );
-    $('#numberMarchePublicFranceAnnee').animateNumber(
-        {number: 200000},
-        1000,
-        function () {
-            $('#numberMarchePublicFranceAnnee').text('200 000')
-        }
-    );
-
-
-    $('.animate-number').each(function () {
-        var $number = $(this);
-        $number.animateNumber(
-            {number: $number.data('number')},
+        $('#numberDematSimple').animateNumber(
+            {number: 100},
+            1000
+        );
+        $('#numberMarchePublicFranceAnnee').animateNumber(
+            {number: 200000},
             1000,
             function () {
-                $number.text($.number( $number.data('number'), 0, ',', ' ' ));
+                $('#numberMarchePublicFranceAnnee').text('200 000')
             }
         );
-    });
 
-    $(document).ready(function () {
-        $('.carousel-logos').carousel('cycle')
+
+        $('.animate-number').each(function () {
+            var $number = $(this);
+            $number.animateNumber(
+                {number: $number.data('number')},
+                1000,
+                function () {
+                    $number.text($.number( $number.data('number'), 0, ',', ' ' ));
+                }
+            );
+        });
+
+        $('.circle').each(function () {
+            var $circle = $(this);
+            $circle.circleProgress().on('circle-animation-progress', function (event, progress) {
+                $(this).find('strong').html($.number( Math.round($circle.data('number') * progress), 0, ',', ' ' ));
+            }).on('circle-animation-end', function () {
+                $(this).find('strong').html($.number( $circle.data('number'), 0, ',', ' ' ));
+            })
+        });
+
     });
 
 
